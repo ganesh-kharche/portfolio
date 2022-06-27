4128aca2f77bc04694dff905badb7f928775defe
ganesh <pre.devpune@gmail.com>
2022-06-27 11:07:25 +0530

    Updates

---
+++ precache-manifest.df91a0b0ab749daf64a41d70e04b9717.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67386b9cf2b8561f58eb7e4edb3b2d8e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "45ac81e76cbc791f5f2f",
+    "url": "/portfolio/static/css/main.857cf3a9.chunk.css"
+  },
+  {
+    "revision": "93f78af6985590b7c20b",
+    "url": "/portfolio/static/js/2.015f2d50.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/portfolio/static/js/2.015f2d50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45ac81e76cbc791f5f2f",
+    "url": "/portfolio/static/js/main.2f6a8f45.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/portfolio/static/js/main.2f6a8f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "ec4e07388caff304f740ce9898edb537",
+    "url": "/portfolio/static/media/ganeshkharche.ec4e0738.png"
+  },
+  {
+    "revision": "7464160a9991c5e564fd18e9fd8bc577",
+    "url": "/portfolio/static/media/nodeJs_HackerRank.7464160a.png"
+  },
+  {
+    "revision": "bbef49cfe24a18a4dffef7edad56de5f",
+    "url": "/portfolio/static/media/resume.bbef49cf.pdf"
+  },
+  {
+    "revision": "c9389937f75df85d83996e1782f9ab0e",
+    "url": "/portfolio/static/media/star_certificate.c9389937.png"
+  }
+]);
